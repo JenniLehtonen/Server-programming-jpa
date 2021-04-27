5c9e4f25309deb1d459e47ce595bd55963d38dde
Sanivu86 <59826845+Sanivu86@users.noreply.github.com>
2021-04-27 13:26:49 +0300

    Peristence created

---
+++ src/main/java/data/Admin.java
@@ -0,0 +1,41 @@
+package data;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+
+/**
+ * The persistent class for the admin database table.
+ * 
+ */
+@Entity
+@NamedQuery(name="Admin.findAll", query="SELECT a FROM Admin a")
+public class Admin implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private String username;
+
+	private String password;
+
+	public Admin() {
+	}
+
+	public String getUsername() {
+		return this.username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return this.password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
