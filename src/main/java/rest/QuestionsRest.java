7a49b04eb26439f90a0c2f7cd10c272d3d327d12
LiisaVuorenmaa <liisa.kuisma@hotmail.fi>
2021-04-28 13:55:09 +0300

    showquestionjsp muokattu ja lisätty daoon kysymysten haku ja rest questioniin MUT EI TOIMI

--- src/main/java/rest/QuestionsRest.java
+++ src/main/java/rest/QuestionsRest.java
@@ -1,7 +1,16 @@
 package rest;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
@@ -16,8 +25,35 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
 import data.*;
+import dao.*;
 
 @Path("/questionrest")
 public class QuestionsRest {
+	@Context HttpServletRequest request;
+	@Context HttpServletResponse response;
+	
+	@POST
+	@Path("/getallquestions")
+	@Produces(MediaType.APPLICATION_JSON)
+	public void getAllCandidates() //throws ServletException, IOException
+	{
+		List<Kysymykset> questionlist = new ArrayList<Kysymykset>();
+		Dao dao = new Dao();
+
+		questionlist = dao.getAllQuestions();
+		
+		
+		request.setAttribute("questionlist", questionlist);
+		RequestDispatcher rd = request.getRequestDispatcher("/jsp/showquestion.jsp");
+		try {
+			rd.forward(request, response);
+		} catch (ServletException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		
+		
+		
+	} 
 
 }
