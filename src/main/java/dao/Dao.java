86677218fd51e40b9e8d627f75ec7a4ca6df2764
Sanivu86 <59826845+Sanivu86@users.noreply.github.com>
2021-05-05 18:31:16 +0300

    Few changes

--- src/main/java/dao/Dao.java
+++ src/main/java/dao/Dao.java
@@ -213,7 +213,7 @@ public class Dao {
 			try
 			{
 				
-				Vastaukset vastaus = em.find(Vastaukset.class, v.getId().getEhdokasId());
+				Vastaukset vastaus = em.find(Vastaukset.class, vastaukset.getId().getEhdokasId());
 
 				if (vastaus != null) 
 				{
